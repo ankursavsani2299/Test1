73b346857ba2fb9d4278fe204bd03ee747dc809b
ankursavsani2299 <ankursavsani22@gmail.com>
2020-01-30 03:38:51 +0530

    Initial commit for few students code Practice2

---
+++ src/test1/Test1.java
@@ -0,0 +1,16 @@
+package test1;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Ankur Savsani
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
